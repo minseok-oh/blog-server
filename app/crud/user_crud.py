82f1b24ae23598f2993da0ac62a2a0f9cd06cd75
정선웅 <tjsdnd1219@naver.com>
2024-11-01 14:38:20 +0900

    회원 가입을 하는 SQL을 작성해야 한다 (#130)
    * feat/12
    
    * feat/12
    
    * fix: create_user 시 id 제거
    
    ---------
    
    Co-authored-by: jeongseonwoong <jeongseonwoong@LAPTOP-JQUTIV0S>
    Co-authored-by: MinSeok Oh <minseok.dev@gmail.com>

--- app/crud/user_crud.py
+++ app/crud/user_crud.py
@@ -6,13 +6,16 @@ from app.mapper import convert_to_user
 from app.models.user_entity import UserEntity
 
 
+def create_user(db: Session, user: User):
+    user_entity = UserEntity(name=user.name, member_id=user.member_id, password=user.password,
+                             role=user.role, avatar=user.avatar, phone_number=user.phone_number,
+                             student_number=user.student_number, birth=user.birth, email=user.email)
+    return db.add(user_entity)
+
+
 def get_user_by_id(db: Session, id: int) -> User:
     """
     id를 이용하여 id에 해당하는 User를 찾습니다.
     """
     user_entity = db.query(UserEntity).filter(UserEntity.id == id).first()
     return convert_to_user(user_entity) if user_entity else None
-
-def encrypt_password(user : UserEntity):
-    user.password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
-    return user
